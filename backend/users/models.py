a2177cf5a0b87804e9a4d6fcbfac1b21887fd1df
Habip ELİŞ <46339672+habipels@users.noreply.github.com>
2023-06-23 01:29:21 +0300

    #database update

--- backend/users/models.py
+++ backend/users/models.py
@@ -65,6 +65,7 @@ class adresler(models.Model):
     semt = models.CharField(blank = True,null = True,max_length=100,verbose_name="Semt")
     ilce = models.CharField(blank = True,null = True,max_length=100,verbose_name="İlçe")
     il = models.CharField(blank = True,null = True,max_length = 50 ,verbose_name="İl",default="Van",choices= data)
+    silinme_bilgisi = models.BooleanField(default=False)
 class vergi_dairesi(models.Model):
     vergi_dairesi_adi = models.CharField(max_length=200,verbose_name="Vewrigi Dairesi")
     vergi_dairesi_kodu = models.CharField(max_length=100,verbose_name="Vergi Dairesi Kodu")
@@ -141,3 +142,74 @@ class sube(models.Model):
     mukellefiyet_turu = models.CharField(max_length=100,blank=True, null=True,verbose_name="Mükellefiyet Türü ",choices=defter_mukelefiyet_turu_secme,default="Gerçek Kişi")
     stok_bilgisi = models.CharField(max_length=100,blank=True, null=True,verbose_name="Stok Envanter Bilgileri",choices=stok_envanter_bilgileri_secme,default="FİFO")
     gecici_vergi_orani = models.BigIntegerField(blank=True, null=True,verbose_name="Geçici Vergi Oranı %")
+    silinme_bilgisi = models.BooleanField(default=False)
+class kategory_link_ayari(models.CharField):
+    def __init__(self, *args, **kwargs):
+        super(kategory_link_ayari, self).__init__(*args, **kwargs)
+    def get_prep_value(self, value):
+        return str(value).lower().replace(" ","_")
+
+class user_adi(models.CharField):
+    def __init__(self, *args, **kwargs):
+        super(user_adi, self).__init__(*args, **kwargs)
+    def get_prep_value(self, value):
+        return str(value).upper()
+class İller_ve_ilceler(models.Model):
+    kategory_tr = user_adi(verbose_name="İl veya İlçe ",max_length=100)
+    ust_kategory = models.ForeignKey('self',blank=True,null=True,verbose_name="İlçe İse İli Şecin",related_name='children',on_delete=models.CASCADE)
+    link = kategory_link_ayari(max_length=200)
+    keywords = models.CharField(max_length=255)
+    def __str__(self):
+        full_path = [self.kategory_tr]                  # post.  use __unicode__ in place of
+        k = self.ust_kategory
+        while k is not None:
+            full_path.append(k.kategory_tr)
+            k = k.ust_kategory
+        return ' --> '.join(full_path[::-1])
+        
+class ortak_bilgileri(models.Model):
+    unvan_secim = (
+        ("işveren","işveren"),
+        ("Yönetici","Yönetici"),
+        ("Ortak","Ortak"),
+        ("İşveren Vekil","İşveren Vekil")
+
+    )
+    vekil_ortak_secim = (
+        ("Ortak","Ortak"),
+        ("İşveren Vekil","İşveren Vekil")
+    )
+    
+    firma_bilgisi = models.ForeignKey(firma,on_delete=models.SET_NULL,blank=True,null=True,verbose_name="Firma Bilgisi")
+    unvan = models.CharField(max_length=100,null=True,blank=True,choices=unvan_secim,default="işveren",verbose_name="Unvan Bilgisi")
+    vekil_ortak= models.CharField(max_length=100,null=True,blank=True,choices=vekil_ortak_secim,default="Ortak",verbose_name="vekil Ortaklık Bilgisi")
+    ortaklik_giris_tarihi = models.DateField(null=True,blank=True,verbose_name="Ortaklık Giriş Tarihi")
+    ortakliktan_cikis_tarihi = models.DateField(null=True,blank=True,verbose_name="Ortaklıktan Çıkış Tarihi")
+    goreve_baslama_tarihi = models.DateField(null=True,blank=True,verbose_name="Göreve Başlama Tarihi")
+    gorevi_bitirme_tarihi = models.DateField(null=True,blank=True,verbose_name="Görevi Bitirme Tarihi")
+    hisse_orani = models.BigIntegerField(null=True,blank=True,verbose_name="Hisse Oranı")
+    hisse_tutari = models.BigIntegerField(null=True,blank=True,verbose_name="Hisse Tutarı")
+    ticaret_sicil_no = models.CharField(max_length=100,null=True,blank=True,verbose_name="Ticaret Sicil No")
+    ticaret_sicil_gazetesi_tarihi = models.DateField(null=True,blank=True,verbose_name="Ticaret Sicil Gazetesi Tarihi")
+    ticaret_sicil_gazetesi_sayfa_n = models.BigIntegerField(null=True,blank=True,verbose_name="Ticaret Sicil Gazetesi Sayfa No")
+    vergi_dairesi_adi = models.ForeignKey(vergi_dairesi,blank=True, null=True,on_delete=models.SET_NULL)
+    vergi_dairesi_kodu = models.CharField(max_length=100,blank=True, null=True,verbose_name="Vergi Dairesi Kodu")
+    vergi_numarasi = models.CharField(max_length=100 ,blank=True, null=True,verbose_name="Vergi Numarası")
+    vergi_dairesi_ili_ilcesi = models.ForeignKey(İller_ve_ilceler,on_delete=models.SET_NULL,blank=True,null=True)
+    silinme_bilgisi = models.BooleanField(default=False)
+class ortak_is_bilgileri(models.Model):
+    tutugu_defter_turu_sec =(
+        ("",""),
+        ("Bilanço","Bilanço"),
+        ("Diğer Defter","Diğer Defter"),
+        ("Deftere Tabi Değil","Deftere Tabi Değil")
+    )
+    ortak_bilgileri = models.ForeignKey(ortak_bilgileri,on_delete=models.CASCADE)
+    bagkur_no = models.CharField(max_length=100,verbose_name="Bağkur Numarası")
+    bagkur_basamagi= models.CharField(max_length=100,verbose_name="Bağkur Başamağı")
+    ssk_no= models.CharField(max_length=100,verbose_name="SSK NO")
+    emekli_sandigi_no = models.CharField(max_length=100,verbose_name="Emekli Sandığı No")
+    diger_isyeri_tescil_no = models.CharField(max_length=100,verbose_name="Diğer İşyeri Tescil No")
+    vakif_no = models.CharField(max_length=100,verbose_name="Vakıf No")
+    dernek_no = models.CharField(max_length=100,verbose_name="Dernek No")
+    tutugu_defter_turu = models.CharField(max_length=100,verbose_name="Tutuğu Defter Türü",choices=tutugu_defter_turu_sec,default="")
