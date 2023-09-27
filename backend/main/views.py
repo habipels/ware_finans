adc1dd369d7ba1370894d2171c151cecf03af36b
Habip ELİŞ <46339672+habipels@users.noreply.github.com>
2023-09-27 22:26:35 +0300

    #firma kasa ekleme update

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse,get_object_or_404
 from django.http import HttpResponse
 from users.models import *
+from bilgideposu.models import *
 # Create your views here.
 def homepage(request):
     content = {}
@@ -22,4 +23,7 @@ def kasa_sayfasi(request,slug):
 def yeni_kasa_karti_sayfasi(request,slug):
     content ={}
     content["firma"] = get_object_or_404(firma,silinme_bilgisi = False,firma_muhasabecisi = request.user,firma_ozel_anahtar = slug)
+    hesaplar = HesapPlanlari.objects.filter(silinme_bilgisi = False,bagli_oldugu_firma =get_object_or_404(firma,silinme_bilgisi = False,firma_muhasabecisi = request.user,firma_ozel_anahtar = slug) )
+    hesaplar.ordered(HesapPlanlari.objects.filter(silinme_bilgisi = False,bagli_oldugu_firma =get_object_or_404(firma,silinme_bilgisi = False,firma_muhasabecisi = None )))
+    content["hesapplanlari"] = hesaplar
     return render(request,"kasa/yenikasa.html",content)
