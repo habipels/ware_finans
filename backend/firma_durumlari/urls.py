dade7eee951f48b12da645bb1299019a6e8677ac
Habip ELİŞ <46339672+habipels@users.noreply.github.com>
2024-01-01 04:28:06 +0300

    #şube ekleme firma silme şube kaydetme olayları

--- backend/firma_durumlari/urls.py
+++ backend/firma_durumlari/urls.py
@@ -8,4 +8,9 @@ app_name = "company"
 urlpatterns = [
     path("companysettings/",firma_ekleme,name="Firma_ekle"),
     path("show/<str:slug>",firma_gosterme,name="firmagosterme"),
+    path("addsite/<str:slug>",firmaya_sube_ekleme,name="firmaya_sube_ekleme"),
+    path("show/<str:slug>/<int:id>",firma_gosterme_subeli,name="firma_gosterme_subeli"),
+    path("delete/<str:slug>/<int:id>",sube_sil,name="sube_sil"),
+    path("deletecompany/<str:slug>",firma_sil,name="firma_sil"),
 ]
+#
